0e45cbf42fab2f0e06b3b4d51dc8f050fbae7734
ksk009 <42211242+009ksk@users.noreply.github.com>
2024-09-01 00:33:36 +0900

    変な文字が入っていたので削除

--- settings/profile.ps1
+++ settings/profile.ps1
@@ -1,10 +1,8 @@
 # Powershellのプロンプトをカスタマイズする
 # C:\Program Files\PowerShell\7\profile.ps1
-```powershell
 function prompt() {
     #カレント・フォルダのパスから末尾の要素のみを抽出
     #[System.Security.Principal.WindowsIdentity]::GetCurrent().Name
     $now = Get-Date -Format "yyyy/MM/dd HH:mm"
     $now + " [ " + (Split-Path (Get-Location) -Leaf) + " ] > "
 }
-```
